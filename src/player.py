5dc5c45279540474e3fcf1ff709d3047740a7b04
Carlos Sanchez <carlo.s16@hotmail.com>
2020-01-24 00:15:36 -0500

    have added base code for both player and room classes

--- src/player.py
+++ src/player.py
@@ -1,2 +1,8 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, player_name, player_room):
+        self.player_room = player_room
+        self.player_name = player_name
+    def __str__(self):
+        return f"{self.player_name} is currently in {self.player_room}"
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, room_name, room_desc):
+        self.room_name = room_name
+        self.room_desc = room_desc
+    def __str__(self):
+        return f"{self.room_name}: {self.room_desc}"
+    # def get_room():
+    # def set_room():
